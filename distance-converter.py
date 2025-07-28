14edea884bae0f7589bbd665fcfe64016a2043e2
Misha Kantaria <my@mail.com>
2025-07-28 20:36:08 +0400

    Initialize converter script

---
+++ distance-converter.py
@@ -0,0 +1,15 @@
+# Distance converter from meter to km and km to meter
+
+ops = input('km or m? ')
+value = int(input('Value: '))
+
+if ops == 'km':
+  pass
+
+elif ops == 'm':
+  pass
+
+else:
+  print("Can't convert!")
+
+
